bc4273b158982a0225742d0717adedbbe58918c2
Bilal <bilal.a.haroon@gmail.com>
2020-04-27 15:28:25 -0500

    added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rasberry Pi Light Indicator
+
+This project is made to run on the rasberry pi.
+
+(https://www.linkedin.com/posts/bilalharoon1604_python-code-javascript-activity-6660632350457688065-yjuE)[linkedin]
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 import RPi.GPIO as GPIO
 import math
-
+# pins: 18, 17, 4 BCM
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,14 +16,26 @@ def index():
     
 @app.route("/led", methods=['POST'])
 def led():  
-    ledOn = request.form.get("led")
-    print(ledOn)
-    if ledOn == "on":
-        GPIO.output(18, GPIO.HIGH)
-    else:
-        GPIO.output(18, GPIO.LOW)
+    light_selector = request.form["light-selector"]
+    switch_light(light_selector)
     return redirect(url_for("index"))
     
+
+
+
+def switch_light(color):
+    light_dict = {
+                "green":[18, False],
+                "red":[17, False],
+                "yellow":[4, False]
+            }
+    for led_color in light_dict:
+        if led_color == color:
+            GPIO.output(light_dict[led_color][0], GPIO.HIGH)
+            light_dict[led_color][1] = True
+        else:
+            GPIO.output(light_dict[led_color][0], GPIO.LOW)
+            light_dict[led_color][1] = False
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True, host="0.0.0.0")
